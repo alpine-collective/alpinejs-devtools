73448498a39affc77fac70f24711aa016e7a28cf
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-04-12 16:30:38 +0100

    make detections actually work

--- packages/shell-chrome/src/env.js
+++ packages/shell-chrome/src/env.js
@@ -1,4 +1,4 @@
-// not used yet but adding in here to make
-// fixing bugs in future easier
+// Lifted from vue-devtools/packages/shared-utils/src/env.js
+export const isBrowser = typeof navigator !== 'undefined'
 export const isChrome = typeof chrome !== 'undefined' && !!chrome.devtools
 export const isFirefox = isBrowser && navigator.userAgent.indexOf('Firefox') > -1
