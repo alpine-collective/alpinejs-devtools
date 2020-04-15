57300e46c893be987a84f9939f547c3bc79b0f6e
Ryan Chandler <ryan@chandler.codes>
2020-04-15 15:37:24 +0100

    Update PurgeCSS to support Tailwind UI

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,9 @@ module.exports = {
         require('@fullhuman/postcss-purgecss')({
             content: [
                 './packages/shell-chrome/assets/*.html'
+            ],
+            whitelistPatterns: [
+                /[\w-/.:]+(?<!:)/g,
             ]
         })
     ]
