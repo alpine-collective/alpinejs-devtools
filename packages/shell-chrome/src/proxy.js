2b67d7a9fd4563416ca317ad8c53c74c44c8bd95
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2020-11-22 13:04:34 +0000

    fix connector duplication issue by wrapping in a closure

--- packages/shell-chrome/src/proxy.js
+++ packages/shell-chrome/src/proxy.js
@@ -3,37 +3,42 @@
 // to the chrome runtime API. It serves as a proxy between the injected
 // backend and the Alpine.js devtools panel.
 
-let connector = chrome.runtime.connect({
-  name: "content-script",
-});
+function proxy() {
+    const connector = chrome.runtime.connect({
+        name: "content-script",
+    });
 
-connector.onMessage.addListener(sendMessageToBackend);
-window.addEventListener("message", sendMessageToDevtools);
-connector.onDisconnect.addListener(handleDisconnect);
+    connector.onMessage.addListener(sendMessageToBackend);
+    window.addEventListener("message", sendMessageToDevtools);
+    connector.onDisconnect.addListener(handleDisconnect);
 
-sendMessageToBackend("init");
+    sendMessageToBackend("init");
 
-function sendMessageToBackend(payload) {
-  window.postMessage(
-    {
-      source: "alpine-devtools-proxy",
-      payload: payload,
-    },
-    "*"
-  );
-}
-
-function sendMessageToDevtools(e) {
-  if (e.data && e.data.source === "alpine-devtools-backend") {
-    connector.postMessage(e.data.payload);
-  } else if (e.data && e.data.source === "alpine-devtools-backend-injection") {
-    if (e.data.payload === "listening") {
-      sendMessageToBackend("init");
+    function sendMessageToBackend(payload) {
+        window.postMessage(
+            {
+                source: "alpine-devtools-proxy",
+                payload: payload,
+            },
+            "*"
+        );
     }
-  }
+
+    function sendMessageToDevtools(e) {
+        if (e.data && e.data.source === "alpine-devtools-backend") {
+            connector.postMessage(e.data.payload);
+        } else if (e.data && e.data.source === "alpine-devtools-backend-injection") {
+            if (e.data.payload === "listening") {
+                sendMessageToBackend("init");
+            }
+        }
+    }
+
+    function handleDisconnect() {
+        connector.onMessage.removeListener(sendMessageToBackend);
+        window.removeEventListener("message", sendMessageToDevtools);
+        sendMessageToBackend("shutdown");
+    }
 }
 
-function handleDisconnect() {
-  window.removeEventListener("message", sendMessageToDevtools);
-  sendMessageToBackend("shutdown");
-}
+proxy();
