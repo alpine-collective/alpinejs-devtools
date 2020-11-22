aa245b818710ddbf20f5de0029a9bd57b592a00c
Hugo <HugoDF@users.noreply.github.com>
2020-11-22 13:19:44 +0000

    whitespace fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@ module.exports = {
   theme: {
     fontSize: {
         'xs': '.75rem',
-  	    'sm': '.83333rem',
+        'sm': '.83333rem',
         'base': '1rem',
         'lg': '1.125rem',
         'xl': '1.25rem',
         '2xl': '1.5rem',
         '3xl': '1.875rem',
-  	    '4xl': '2.25rem',
+        '4xl': '2.25rem',
         '5xl': '3rem',
         '6xl': '4rem',
     },
