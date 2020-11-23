275d45fae4e1c30eda3dddcc403fd65dae31d792
Hugo <HugoDF@users.noreply.github.com>
2020-11-23 19:47:36 +0000

    Fix: tailwind build, remove terser (#66)
    * upgrade rollup-plugin-postcss
    
    * extract styles to styles.css + load them in panel
    
    * configure Tailwind to purge
    
    * update package.json description + repo
    
    * remove terser plugin (saves 52k)
    
    with terser: 148K
    without terser: 200K
    
    considering we were shipping the whole of Tailwind inside of a JS bundle...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,27 @@
 module.exports = {
-  theme: {
-    fontSize: {
-        'xs': '.75rem',
-        'sm': '.83333rem',
-        'base': '1rem',
-        'lg': '1.125rem',
-        'xl': '1.25rem',
-        '2xl': '1.5rem',
-        '3xl': '1.875rem',
-        '4xl': '2.25rem',
-        '5xl': '3rem',
-        '6xl': '4rem',
+    purge: {
+        content: ['./packages/**/*.html'],
     },
+    theme: {
+        fontSize: {
+            'xs': '.75rem',
+            'sm': '.83333rem',
+            'base': '1rem',
+            'lg': '1.125rem',
+            'xl': '1.25rem',
+            '2xl': '1.5rem',
+            '3xl': '1.875rem',
+            '4xl': '2.25rem',
+            '5xl': '3rem',
+            '6xl': '4rem',
+        },
 
-    extend: {},
-  },
-  variants: {
-  	opacity: ['responsive', 'hover', 'focus', 'group-hover'],
-  },
-  plugins: [
-    require('@tailwindcss/ui'),
-  ],
+        extend: {},
+    },
+    variants: {
+        opacity: ['responsive', 'hover', 'focus', 'group-hover'],
+    },
+    plugins: [
+        require('@tailwindcss/ui'),
+    ],
 }
