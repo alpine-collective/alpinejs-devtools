6a80d2199777783d0d7540a5ce6dc0f46ea5dd6c
Kevin Batdorf <kbat82@gmail.com>
2020-12-06 21:33:47 +0700

    update tests

--- cypress/integration/settings.js
+++ cypress/integration/settings.js
@@ -1,12 +1,13 @@
 it('should be able to open and close the settings modal', () => {
     cy.visit('/')
         .get('[data-testid=component-name]')
-        .get('#settingsPanelOpenButton')
-        .click()
-        .get('#settingsPanel')
+        .get('[data-testid=settings-panel-open-button')
         .should('be.visible')
-        .get('#settingsPanelCloseButton')
+        .click()
+        .get('[data-testid=settings-panel')
+        .should('be.visible')
+        .get('[data-testid=settings-panel-close-button')
         .click({ force: true }) // Have to force it because of the absolute positioning
-        .get('#settingsPanel')
+        .get('[data-testid=settings-panel')
         .should('not.be.visible')
 })
