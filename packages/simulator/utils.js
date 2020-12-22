450ce706eb957ef7ae87617cb235ae15a227533d
Kevin Batdorf <kbat82@gmail.com>
2020-12-22 19:47:18 +0700

    update

--- packages/simulator/utils.js
+++ packages/simulator/utils.js
@@ -15,7 +15,7 @@ export async function injectPanel(containerNode) {
     // this contains all sorts of CSS tags etc
     containerNode.innerHTML = rawPanelHtml
     // keep only the Alpine components in the panel
-    const panelAppHtml = Array.from(containerNode.querySelectorAll('#devtools-container > [x-data]'))
+    const panelAppHtml = Array.from(containerNode.querySelectorAll('[data-testid="panel-root"]'))
         .map((el) => el.outerHTML)
         .join('\n')
 
