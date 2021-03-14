b79d687c23c60c1ff9b9ffd799ce9d861c537bee
Hugo <HugoDF@users.noreply.github.com>
2021-03-14 11:06:36 +0000

    perf (attribute-x-for-rendering) (#182)
    * perf(attr-x-if-instead-of-show)
    
    * fix(cypress-tests)
    
    not.be.visible -> not.exist
    
    * refactor(remove-unnecessary-next-tick)
    
    * test(remove-only)

--- cypress/integration/component.js
+++ cypress/integration/component.js
@@ -169,10 +169,10 @@ it('should display read-only function/HTMLElement attributes + allow editing of 
     // check they toggle off
     cy.get('[data-testid=data-property-value-el]').click()
 
-    cy.get('[data-testid=data-property-value-name]').should('not.be.visible')
+    cy.get('[data-testid=data-property-value-name]').should('not.exist')
 
-    cy.get('[data-testid=data-property-name-attributes]').should('not.be.visible')
-    cy.get('[data-testid=data-property-name-children]').should('not.be.visible')
+    cy.get('[data-testid=data-property-name-attributes]').should('not.exist')
+    cy.get('[data-testid=data-property-name-children]').should('not.exist')
 
     // booleans
     cy.get('[data-testid=data-property-name-bool]').should('be.visible').contains('bool')
@@ -232,32 +232,32 @@ it('should display nested arrays/object attributes and support editing', () => {
 
     cy.get('[data-testid=data-property-value-array]').click()
 
-    cy.get('[data-testid=data-property-name-0]').last().should('be.visible').contains('0')
-    cy.get('[data-testid=data-property-value-0]').last().should('be.visible').contains('Object')
+    cy.get('[data-testid=data-property-name-0]').should('be.visible').contains('0')
+    cy.get('[data-testid=data-property-value-0]').should('be.visible').contains('Object')
 
-    cy.get('[data-testid=data-property-name-0]').last().click()
+    cy.get('[data-testid=data-property-name-0]').click()
 
     cy.get('[data-testid=data-property-name-nested]').should('be.visible').contains('nested')
     cy.get('[data-testid=data-property-value-nested]').should('be.visible').contains('property')
 
     // check untoggling also works
-    cy.get('[data-testid=data-property-name-0]').last().click()
+    cy.get('[data-testid=data-property-name-0]').click()
 
-    cy.get('[data-testid=data-property-name-nested]').should('not.be.visible')
-    cy.get('[data-testid=data-property-value-nested]').should('not.be.visible')
+    cy.get('[data-testid=data-property-name-nested]').should('not.exist')
+    cy.get('[data-testid=data-property-value-nested]').should('not.exist')
 
     cy.get('[data-testid=data-property-value-array]').click()
 
-    cy.get('[data-testid=data-property-name-0]').last().should('not.be.visible')
-    cy.get('[data-testid=data-property-value-0]').last().should('not.be.visible')
+    cy.get('[data-testid=data-property-name-0]').should('not.exist')
+    cy.get('[data-testid=data-property-value-0]').should('not.exist')
 
     cy.get('[data-testid="data-property-value-nestedObjArr"]').click()
-    cy.get('[data-testid=data-property-name-array]').should('not.be.visible')
-    cy.get('[data-testid=data-property-value-array]').should('not.be.visible')
+    cy.get('[data-testid=data-property-name-array]').should('not.exist')
+    cy.get('[data-testid=data-property-value-array]').should('not.exist')
 
     cy.get('[data-testid="data-property-value-nestedObjArr"]').click()
     cy.get('[data-testid=data-property-value-array]').click()
-    cy.get('[data-testid=data-property-name-0]').last().click()
+    cy.get('[data-testid=data-property-name-0]').click()
     // editing the nested array/object
     cy.get('[data-testid=edit-icon-nested]').click({ force: true })
     cy.get('[data-testid=input-nested]')
